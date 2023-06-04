c3a0eaa65f5a01971c75de93f3183fc1eae4fa09
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-06-04 10:09:28 +0200

    Clean qtile config file

--- private_dot_config/qtile/config.py
+++ private_dot_config/qtile/config.py
@@ -1,28 +1,13 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+#   █████  ▄▄▄█████▓ ██▓ ██▓    ▓█████ 
+# ▒██▓  ██▒▓  ██▒ ▓▒▓██▒▓██▒    ▓█   ▀ 
+# ▒██▒  ██░▒ ▓██░ ▒░▒██▒▒██░    ▒███   
+# ░██  █▀ ░░ ▓██▓ ░ ░██░▒██░    ▒▓█  ▄ 
+# ░▒███▒█▄   ▒██▒ ░ ░██░░██████▒░▒████▒
+# ░░ ▒▒░ ▒   ▒ ░░   ░▓  ░ ▒░▓  ░░░ ▒░ ░
+#  ░ ▒░  ░     ░     ▒ ░░ ░ ▒  ░ ░ ░  ░
+#   ░   ░   ░       ▒ ░  ░ ░      ░   
+#    ░              ░      ░  ░   ░  ░
+                                     
 
 from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
@@ -31,7 +16,6 @@ from libqtile.lazy import lazy
 import datetime
 import os
 import subprocess
-import time
 
 mod = "mod4"
 terminal = "kitty"
@@ -42,47 +26,45 @@ def autostart():
     home = os.path.expanduser('~/.config/qtile/autostart.sh')
     subprocess.call([home])
 
-
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+
     # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
+
+    # Grow windows
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
+
     Key(
         [mod, "shift"],
         "Tab",
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+
+
     # Toggle between different layouts as defined below
     Key([mod], "m", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "x", lazy.window.kill(), desc="Kill focused window"),
+
+    
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "p", lazy.window.toggle_floating(), desc="Toggle floating",),
 
+    # Rofi
     Key([mod], "d", lazy.spawn("rofi -show drun")),
     Key([mod], "f", lazy.spawn("rofi -show window")),
     Key([mod], "g", lazy.spawn("rofi -show filebrowser")),
@@ -90,23 +72,20 @@ keys = [
     Key([mod], "s", lazy.spawn("/home/adiantum/.config/rofi/select_spotify_playlist/rofi_spotify_playlist.sh")),
     
 
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     Key([mod], "t", lazy.spawn(terminal)),
-    Key([mod, "shift"], "space", lazy.group.toggle_all_fullscreen()),
+
+    # Media control
     Key([], "XF86AudioLowerVolume",lazy.spawn("amixer -c 1 sset PCM 2%-")),
     Key([], "XF86AudioRaiseVolume",lazy.spawn("amixer -c 1 sset PCM 2%+")),
     Key([], "XF86AudioPrev",lazy.spawn("playerctl previous")),
     Key([], "XF86AudioNext",lazy.spawn("playerctl next")),
     Key([], "XF86AudioPlay",lazy.spawn("playerctl --player=spotify play-pause")),
 
-    Key([mod], "p",
-        lazy.window.toggle_floating(),
-        desc="Toggle floating",
-        ),
     Key([mod], "o", lazy.widget["keyboardlayout"].next_keyboard(), desc="Next keyboard layout."),
-
 ]
 
-#Colors
+# Colors
 catppuccin = {
     "flamingo": "#F3CDCD",
     "mauve": "#DDB6F2",
@@ -124,10 +103,7 @@ catppuccin = {
     "black": "#1a1826",
         }
 
-
-# groups = [Group(i) for i in "123456789"]
-
-groups = [Group("q",  label="  ", layout="Columns"),
+groups = [Group("q",  label="  "),
                    
           Group("w",  label=" 󰈹 ", spawn="firefox"),
                    
@@ -135,7 +111,7 @@ groups = [Group("q",  label="  ", layout="Columns"),
 
           Group("1",  label="  "),
 
-          Group("2",  label="  ", layout="Columns"),
+          Group("2",  label="  "),
 
           Group("3",  label="  ", spawn="google-chrome https://mail.google.com/mail/u/1/#inbox"),
 
@@ -158,15 +134,10 @@ for i in groups:
                 lazy.window.togroup(i.name, switch_group=True),
                 desc="Switch to & move focused window to group {}".format(i.name),
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
 
 layouts = [
-    # layout.Columns(border_focus_stack=["#6e2fc2", "#712d8c"], border_width=3),
     layout.Columns(
         border_focus=catppuccin["teal"],
         border_normal=catppuccin["black"],
@@ -175,17 +146,6 @@ layouts = [
         margin=8,
         ),
     layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(ratio=0.6, border_focus=catppuccin["teal"], border_normal=catppuccin["black"], border_width=1),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
 ]
 
 widget_defaults = dict(
@@ -196,18 +156,18 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
-# add white space easely in the bottom bar
+# add white space easily in the bottom bar
 def add_white_space(background_color, space_number=1):
     return widget.TextBox(text=" " * space_number, background=background_color )
-circle_size = 35
+
+# define half circle size in bar
+CIRCLE_SIZE = 35
 
 screens = [
     Screen(
 
         bottom=bar.Bar(
             [
-
-                # widget.CurrentLayout(),
                 add_white_space(catppuccin["green"]),
 
                 widget.QuickExit(
@@ -221,7 +181,7 @@ screens = [
                 widget.TextBox(
                     text="",
                     padding=0,
-                    fontsize=circle_size,
+                    fontsize=CIRCLE_SIZE,
                     foreground=catppuccin["green"],
                     background=catppuccin["mauve"],
                     ),
@@ -237,13 +197,11 @@ screens = [
                 widget.TextBox(
                     text="",
                     padding=0,
-                    fontsize=circle_size,
+                    fontsize=CIRCLE_SIZE,
                     foreground=catppuccin["mauve"],
                     background=catppuccin["black"],
                     ),
 
-
-                # widget.Prompt(),
                 widget.WindowName(
                     format="{state}",
                     background=catppuccin["black"],
@@ -260,15 +218,13 @@ screens = [
                 widget.Systray(
                     background=catppuccin["black"],
                     ),
-                # widget.PulseVolume(background=catppuccin["green"], foreground=catppuccin["black"]),
-
 
                 add_white_space(catppuccin["black"]),
 
                 widget.TextBox(
                     text="",
                     padding=0,
-                    fontsize=circle_size,
+                    fontsize=CIRCLE_SIZE,
                     foreground=catppuccin["sky"],
                     background=catppuccin["black"],
                     ),
@@ -302,7 +258,7 @@ screens = [
                 widget.TextBox(
                         text="",
                         padding=0,
-                        fontsize=circle_size,
+                        fontsize=CIRCLE_SIZE,
                         foreground=catppuccin["maroon"],
                         background=catppuccin["sky"],
                         ),
@@ -321,7 +277,6 @@ screens = [
 
                 add_white_space(catppuccin["maroon"]),
 
-
                 widget.Wttr(
                         location={'Lorient': 'Lorient'},
                         background=catppuccin["maroon"],
@@ -329,18 +284,17 @@ screens = [
                         format="%t%c",),
 
                 add_white_space(catppuccin["maroon"]),
-
-
             ],
+
+            # Bar height
             35,
 
             # background="catppuccin["maroon"]",
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            # border_width=[5, 0, 5, 0],  # Draw top and bottom borders
+            # border_color=[catppuccin["black"], "000000",catppuccin["black"], "000000"]  # Borders are magenta
         ),
-       # wallpaper='~/.config/qtile/wallpaper/cat_desk1080.png',
+        # wallpaper='~/.config/qtile/wallpaper/cat_desk1080.png',
         # wallpaper_mode='fill',
-
     ),
 ]
 
@@ -381,12 +335,6 @@ auto_minimize = True
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
