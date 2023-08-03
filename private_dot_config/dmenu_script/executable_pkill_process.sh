c954fed3861e1e2129e887e79cec6555db295d1d
th-herve <thibault.herve1@gmail.com>
2023-08-03 19:16:33 +0200

    Track dmenu script to kill process

---
+++ private_dot_config/dmenu_script/executable_pkill_process.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Note: currently using pkill, using kill might be better?
+
+# Filter out system processes, display it in dmenu 
+process=$(ps -e --format comm -u $USER | grep -vE 'systemd|init|kthreadd|rcu_sched' | sort | uniq | dmenu -fn  'JetBrainsMono Nerd Font:size=14' -nf '#cad3f5'  -nb '#1a1826' -sb '#96cdfb' -sf '#1a1826'  -p '󰚌 ')
+
+# Verify that a process was selected before attempting to kill it
+if [ -n "$process" ]; then
+    # Confirmation prompt before killing the selected process
+    response=$(echo -e "cancel\nterm\nkill" | dmenu -p "Signal $process?" -fn  'JetBrainsMono Nerd Font:size=14' -nf '#cad3f5'  -nb '#1a1826' -sb '#96cdfb' -sf '#1a1826') # You may customize the prompt text here
+
+    if [ "$response" = "term" ]; then
+        pkill -15 "$process"
+    elif [ "$response" = "kill" ]; then
+        pkill -9 "$process"
+    fi
+fi
