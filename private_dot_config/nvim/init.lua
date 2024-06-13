43265610ebd243c7eef744bb868430113994b043
th-herve <thibault.herve1@gmail.com>
2024-06-13 19:15:07 +0200

    Update telescope keybinds

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -732,8 +732,11 @@ local on_attach = function(_, bufnr)
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
   nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ds', function()
+    require('telescope.builtin').lsp_document_symbols { symbols = 'function' }
+  end, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('<leader>fh', require('telescope.builtin').help_tags, '[F]ind [H]elp tags')
 
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
