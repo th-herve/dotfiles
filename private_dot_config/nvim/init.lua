1c7f248fee0822d84a3a6fb9eefdb8a13b7bdb74
th-herve <thibault.herve1@gmail.com>
2024-07-08 00:03:10 +0200

    Update nvim which-key conf

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -835,8 +835,9 @@ require('which-key').register {
   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  ['<leader>w'] = { name = '[W]iki', _ = 'which_key_ignore' },
   ['<leader>f'] = { name = '[F]uzzy find', _ = 'which_key_ignore' },
+  ['<leader>o'] = { name = 'Lang', _ = 'which_key_ignore' },
 }
 
 -- luasnip keybind
