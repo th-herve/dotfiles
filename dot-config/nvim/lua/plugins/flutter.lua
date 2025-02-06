945c04bdbf89e9f679175abf8711f199f5172a51
th-herve <126357296+th-herve@users.noreply.github.com>
2025-02-06 17:05:34 +0100

    Add nvim lsp config in flutter plugin config
    Flutter plugin should use them by default, but is not use them for some reason

--- dot-config/nvim/lua/plugins/flutter.lua
+++ dot-config/nvim/lua/plugins/flutter.lua
@@ -8,7 +8,38 @@ return {
   config = function()
     require('telescope').load_extension 'flutter'
 
-    key('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' }) -- remap because the one set in the lsp is not applied (dk why)
+    local map = function(keys, func, desc)
+      if desc then
+        desc = 'LSP: ' .. desc
+      end
+      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    -- +-------------------------------+
+    -- |           Keybinds            |
+    -- +-------------------------------+
+
     key('n', '<leader>fl', require('telescope').extensions.flutter.commands, { desc = '[C]ode [A]ction' })
+
+    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    map('K', vim.lsp.buf.hover, 'Hover Documentation')
+    map('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+    -- telescope lsp keybinds
+    local ok, telescope = pcall(require, 'telescope.builtin')
+    if ok then
+      map('gd', telescope.lsp_definitions, '[G]oto [D]efinition')
+      map('gr', telescope.lsp_references, '[G]oto [R]eferences')
+      map('gI', telescope.lsp_implementations, '[G]oto [I]mplementation')
+      map('<leader>D', telescope.lsp_type_definitions, 'Type [D]efinition')
+      map('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+      map('<leader>fs', telescope.lsp_document_symbols, '[F]ind [S]ymbols')
+      map('<leader>fh', telescope.help_tags, '[F]ind [H]elp tags')
+      map('<leader>ds', function()
+        telescope.lsp_document_symbols { symbols = 'function' }
+      end, '[D]ocument [S]ymbols')
+    end
   end,
 }
