bc638dabedbe4723b45475ac0610e2863b7c7d9f
th-herve <126357296+th-herve@users.noreply.github.com>
2025-03-26 09:12:14 +0100

    Add nvim go plugin

---
+++ dot-config/nvim/lua/plugins/go.lua
@@ -0,0 +1,25 @@
+return {
+  'ray-x/go.nvim',
+  dependencies = { -- optional packages
+    'ray-x/guihua.lua',
+    'neovim/nvim-lspconfig',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = function()
+    require('go').setup()
+
+    local map = function(keys, func, desc)
+      if desc then
+        desc = 'LSP: ' .. desc
+      end
+      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    map('<leader>gf', ':GoFillStruct<CR>', '[g]o [f]ill struct')
+    map('<leader>ge', ':GoIfErr<CR>', '[g]o if [e]rr')
+
+  end,
+  event = { 'CmdlineEnter' },
+  ft = { 'go', 'gomod' },
+  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+}
