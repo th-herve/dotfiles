70384844ead7675e29f080801a5db2830cf16bda
th-herve <126357296+th-herve@users.noreply.github.com>
2025-05-01 21:35:44 +0200

    Remove nvim notify
    Causes some highlight errors

--- dot-config/nvim/lua/plugins/noice.lua
+++ dot-config/nvim/lua/plugins/noice.lua
@@ -10,7 +10,7 @@ return {
       lsp_doc_border = true, -- add a border to hover docs and signature help
     },
     notify = {
-      enabled = true,
+      enabled = false,
     },
     messages = {
       enabled = false,
