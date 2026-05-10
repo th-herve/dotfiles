dde5ba80df85e27074e5f7f78b22110b495d12e0
th-herve <126357296+th-herve@users.noreply.github.com>
2026-05-10 18:05:07 +0200

    Add nvim zenmode

--- dot-config/nvim/lua/plugins/init.lua
+++ dot-config/nvim/lua/plugins/init.lua
@@ -4,12 +4,13 @@ return {
   'tpope/vim-repeat',
 
   'mg979/vim-visual-multi',
+  "folke/zen-mode.nvim",
 
   { 'windwp/nvim-autopairs', event = 'InsertEnter', config = true },
 
   { 'numToStr/Comment.nvim', opts = {} },
 
-  { 'th-herve/sline-nvim', opts = {} },
+  { 'th-herve/sline-nvim',   opts = {} },
 
   -- require 'debug.debug',
 
