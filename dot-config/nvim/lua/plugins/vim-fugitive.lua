ea8eab6d36373581b3f1b19243bfc114b693b6d9
th-herve <126357296+th-herve@users.noreply.github.com>
2026-05-18 10:30:06 +0200

    fix(nvim): number col disapearing after closing git fugitive

--- dot-config/nvim/lua/plugins/vim-fugitive.lua
+++ dot-config/nvim/lua/plugins/vim-fugitive.lua
@@ -61,8 +61,12 @@ return {
     end
     key('n', ld .. 'gs', toggleFugitiveGit, opts) -- has to use the leader key, otherwise it won't close
 
-    vim.cmd [[
-        autocmd BufNewFile COMMIT_EDITMSG exec set nonumber norelativenumber
-      ]]
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "fugitive", "gitcommit" },
+      callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+      end,
+    })
   end,
 }
